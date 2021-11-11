14c193424910526e38642213207f6435b9e93dcb
Createitv <xfy150150@163.com>
2021-11-11 12:41:01 +0800

    vue v-if

--- 08-Node.js/Educative/Intro-nodejs/test.js
+++ 08-Node.js/Educative/Intro-nodejs/test.js
@@ -1,10 +1,13 @@
-process.on("beforeExit", () => {
-	console.log("blue");
+// 同步阻塞
+var fs = require("fs");
+var data = fs.readFileSync("./01-event-loop.js");
+// console.log(data.toString());
+
+// 异步非阻塞
+fs.readFile("./01-event-loop.js", function (err, data) {
+	if (err) throw err;
+	console.log(data.toString());
 });
 
-process.on("exit", () => {
-	console.log("green");
-});
-
-console.log("red");
-process.exit();
+// 先输出程序执行完毕再打印程序内容
+console.log("程序执行完毕");
